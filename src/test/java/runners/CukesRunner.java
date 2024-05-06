a1a8f19959b291ba8cce096ec78a448cc7f6e7d3
Gulten_Cydeo <g73h28@gmail.com>
2024-05-06 14:42:55 -0400

    Initial commit

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features" ,
+        glue ="src/test/java/stepDefinitions",
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+}
